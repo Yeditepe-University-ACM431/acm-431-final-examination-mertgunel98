607c9851ed3efe39e9af40910f2dc2604b73d744
mertgunel98 <mertgunel55@gmail.com>
2026-01-06 15:41:02 +0300

    Implement TaskDetailScreen to display task details

--- app/src/main/java/com/yeditepe/finalexam/ui/TaskDetailScreen.kt
+++ app/src/main/java/com/yeditepe/finalexam/ui/TaskDetailScreen.kt
@@ -8,8 +8,48 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun TaskDetailScreen(title: String) {
+    recipeId: Int,
+    viewModel: TaskViewModel = viewModel()
+    ) {
 
-    Column(modifier = Modifier.padding(16.dp)) {
-        // TODO 4: Display task title
+        val task = viewModel.getTaskById(taskId)
+
+        if (task != null) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFFFFFBFE))
+                    .padding(16.dp)
+            ) {
+
+                Text(
+                    text = "Task Detail",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Medium,
+                    color = Color(0xFFD0BCFF),
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+
+
+                Text(
+                    text = task.title,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFFCCC2DC)
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+
+                Text(
+                    text = task.description,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = Color(0xFFCCC2DC)
+                )
+            }
+        }
+        else {
+            Text("Task not found", modifier = Modifier.padding(16.dp))
+        }
     }
-}
